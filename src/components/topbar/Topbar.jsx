ac4a4e8f868ff1a6b8b9488747576913592c8f2a
Bao Phung Gia <bao.phung@onemount.com>
2022-01-20 20:40:01 +0700

    first

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Search } from '@material-ui/icons';
+import { Person, Chat, Notifications } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+
+import './topbar.css';
+
+export default function Topbar() {
+  return (
+    <div className='topbarContainer'>
+      <div className='topbarLeft'>
+        <Link to="/" style={{textDecoration: 'none'}}>
+          <span className='logo'>Bao social</span>
+        </Link>
+      </div>
+      <div className='topbarCenter'>
+        <div className='searchbar'>
+          <Search className='searchIcon' />
+          <input placeholder='search for friend, post or video' className='searchInput' />
+        </div>
+      </div>
+      <div className='topbarRight'>
+        <div className='topbarLinks'>
+          <span className='topbarLink'>Homepage</span>
+          <span className='topbarLink'>Timeline</span>
+        </div>
+        <div className='topbarIcons'>
+          <div className='topbarIconItem'>
+            <Person />
+            <span className='topbarIconBadge'>1</span>
+          </div>
+          <div className='topbarIconItem'>
+            <Chat />
+            <span className='topbarIconBadge'>2</span>
+          </div>
+          <div className='topbarIconItem'>
+            <Notifications />
+            <span className='topbarIconBadge'>3</span>
+          </div>
+        </div>
+        <img src='/assets/person/1.jpeg' alt='' className='topbarImg' />
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Feed from '../../components/feed/Feed';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Topbar from '../../components/topbar/Topbar';
+import Rightbar from '../../components/rightbar/Rightbar';
+import './profile.css';
+
+export default function Profile() {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  return (
+    <>
+      <Topbar />
+      <div className='profile'>
+        <Sidebar />
+        <div className='profileRight'>
+          <div className='profileRightTop'>
+            <div className='profileCover'>
+              <img className='profileCoverImg' src={`${PF}post/3.jpeg`} alt='' />
+              <img className='profileUserImg' src={`${PF}person/7.jpeg`} alt='' />
+            </div>
+            <div className='profileInfo'>
+              <h4 className='profileInfoName'>Bao Phung</h4>
+              <span className='profileInfoDesc'>Hello my friend</span>
+            </div>
+          </div>
+          <div className='profileRightBottom'>
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
